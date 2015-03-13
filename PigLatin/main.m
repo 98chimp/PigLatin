9c820c7d9e6b0f434774a7f31934dcd69e584638
Shahin Zangenehpour <98chimp@users.noreply.github.com>
2015-03-13 16:10:52 -0700

    Initial Commit

---
+++ PigLatin/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PigLatin
+//
+//  Created by Shahin on 2015-03-13.
+//  Copyright (c) 2015 98% Chimp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
